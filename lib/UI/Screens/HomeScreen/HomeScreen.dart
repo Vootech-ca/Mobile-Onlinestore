3458273b7f0015a00b4fd62f6e6184abe453e2dd
Mohammad hassan <mohammad.h.karem@gmail.con>
2021-05-24 18:22:09 +0300

    update home page ui fixes #18

--- lib/UI/Screens/HomeScreen/HomeScreen.dart
+++ lib/UI/Screens/HomeScreen/HomeScreen.dart
@@ -85,75 +85,31 @@ class HomeScreen extends StatelessWidget {
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Text(
-                      "Categories",
-                      style: theme(context).textTheme.headline5,
-                    ),
-                    Spacer(),
-                    GestureDetector(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Text(
-                          "See All",
-                          style: theme(context).textTheme.bodyText2,
-                        ),
-                      ),
-                      onTap: () => Navigator.pushNamed(
-                          context, CategoriesScreen.routeCategoriesScreen),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                  height: 220,
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: [
-                            categoryWidget(context),
-                            categoryWidget(context),
-                            categoryWidget(context),
-                            categoryWidget(context),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: [
-                            categoryWidget(context),
-                            categoryWidget(context),
-                            categoryWidget(context),
-                            categoryWidget(context),
-                          ],
-                        ),
-                      ),
-                    ],
-                  )),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
                 child: Text(
-                  "Items",
+                  "Categories",
                   style: theme(context).textTheme.headline5,
                 ),
               ),
-              SpGrid(
-                width: Responsive.sW(context),
-                spacing: 15,
-                runSpacing: 15,
+              SizedBox(
+                height: 10,
+              ),
+              Column(
                 children: [
-                  ProductCard(context),
-                  ProductCard(context),
-                  ProductCard(context),
-                  ProductCard(context),
-                  ProductCard(context),
-                  ProductCard(context),
-                  ProductCard(context),
-                  ProductCard(context),
+                  SpGrid(
+                    width: Responsive.sW(context),
+                    spacing: 15,
+                    runSpacing: 15,
+                    children: [
+                      categoryWidget(context),
+                      categoryWidget(context),
+                      categoryWidget(context),
+                      categoryWidget(context),
+                      categoryWidget(context),
+                      categoryWidget(context),
+                      categoryWidget(context),
+                      categoryWidget(context),
+                    ],
+                  ),
                 ],
               ),
             ],
@@ -161,28 +117,37 @@ class HomeScreen extends StatelessWidget {
         ));
   }
 
-  Widget categoryWidget(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          margin: EdgeInsets.only(top: 45, right: 10, left: 10, bottom: 10),
-          height: 80,
-          width: 190,
-          decoration: BoxDecoration(
-              color: theme(context).accentColor,
-              borderRadius: BorderRadius.circular(18)),
-        ),
-        Container(
-          height: 80,
-          width: 170,
-          margin: EdgeInsets.all(20),
-          child: CachedNetworkImage(
-            fit: BoxFit.fill,
-            imageUrl: fruitsImageDummy,
-            placeholder: (context, url) => Image.asset(Constants.placeHolder),
+  SpGridItem categoryWidget(BuildContext context) {
+    return SpGridItem(
+      xs: 4,
+      sm: 3,
+      md: 3,
+      child: Column(
+        children: [
+          Container(
+            height: 80,
+            width: 80,
+            decoration: BoxDecoration(
+              border: Border.all(color: theme(context).primaryColor, width: 2),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            margin: EdgeInsets.all(4),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: CachedNetworkImage(
+                fit: BoxFit.cover,
+                imageUrl: fruitsImageDummy,
+                placeholder: (context, url) =>
+                    Image.asset(Constants.placeHolder),
+              ),
+            ),
           ),
-        )
-      ],
+          Text(
+            'Fruits',
+            style: textTheme(context).bodyText2,
+          )
+        ],
+      ),
     );
   }
 }
--- lib/dummyData.dart
+++ lib/dummyData.dart
@@ -7,13 +7,16 @@ final List dCategories = [
   'Electronic',
 ];
 
-final String fruitImageDummy = "https://clipart-best.com/img/tomato/tomato-clip-art-6.png";
-final String productImageDummy = "https://sc04.alicdn.com/kf/HTB1XZmDJFXXXXaAXFXX760XFXXXb.png";
-final String adImageDummy = "https://d540vms5r2s2d.cloudfront.net/mad/uploads/mad_blog_5ebae3ae3850e1589306286.png";
-final String fruitsImageDummy = "https://i.dlpng.com/static/png/7035720_preview.png";
+final String fruitImageDummy =
+    "https://clipart-best.com/img/tomato/tomato-clip-art-6.png";
+final String productImageDummy =
+    "https://sc04.alicdn.com/kf/HTB1XZmDJFXXXXaAXFXX760XFXXXb.png";
+final String adImageDummy =
+    "https://d540vms5r2s2d.cloudfront.net/mad/uploads/mad_blog_5ebae3ae3850e1589306286.png";
+final String fruitsImageDummy =
+    "https://i.dlpng.com/static/png/7035720_preview.png";
 
-
-final List<String> adImagesList= [
+final List<String> adImagesList = [
   adImageDummy,
   adImageDummy,
   adImageDummy,
