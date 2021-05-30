659c54aeb9ca402232bf0c7930abfd22d35597b2
Muhammad M Bakr <mahammadbakr77@gmail.com>
2021-05-30 15:40:17 +0300

    model update for images

--- lib/Models/CategoryModel.dart
+++ lib/Models/CategoryModel.dart
@@ -1,18 +1,21 @@
 class Category {
   final int id;
   final String categoryName;
+  final String categoryImage;
   final DateTime createdAt;
   final DateTime updatedAt;
 
   Category(
       {required this.id,
       required this.categoryName,
+      required this.categoryImage,
       required this.createdAt,
       required this.updatedAt});
 
   factory Category.fromJson(Map<String, dynamic> data) => Category(
       id: data["id"] ?? 0,
-      categoryName: data["category_name"] ?? "NULL",
+      categoryName: data["category_name"] ?? "NULL(Server Error)",
+      categoryImage: data["category_image"] ?? "https://trirama.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.png",
       createdAt: DateTime.parse(data["created_at"]) ,
       updatedAt: DateTime.parse(data["updated_at"])
   );
@@ -20,6 +23,7 @@ class Category {
   Map<String, dynamic> toJson() => <String, dynamic>{
         'id': id,
         'category_name': categoryName,
+        'category_image': categoryImage,
         'created_at': createdAt,
         'updated_at': updatedAt
       };
